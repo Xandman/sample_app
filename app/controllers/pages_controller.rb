e55da96845600a134a873c295d512faa37b7696c
Xandman <agp@arribatel.com>
2010-10-20 09:04:15 +0800

    added @base_title

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,23 @@
 class PagesController < ApplicationController
+  # @base_title = "Ruby on Rails Tutorial"
+  
   def home
+    @base_title = "Ruby on Rails Tutorial"
     @title = "Home"
   end
 
   def contact
+    @base_title = "Ruby on Rails Tutorial"
     @title = "Contact"
   end
 
   def about
+    @base_title = "Ruby on Rails Tutorial"
     @title = "About"
   end
   
   def help
+    @base_title = "Ruby on Rails Tutorial"
     @title = "Help"
   end
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+    #
+   @base_title = "Ruby on Rails Tutorial"
+    #
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -12,7 +18,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title", 
-      :content => "Ruby on Rails Tutorial Sample App | Home")
+      :content => @base_title + " | Home")
                 end
   end
 
@@ -24,7 +30,7 @@ describe PagesController do
     
       it "should have the right title" do
         get 'contact'
-        response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+        response.should have_selector("title", :content => @base_title + " | Contact")
                   end
   end
 
@@ -36,7 +42,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", :content => @base_title + " | About")
                 end
   end
     
@@ -48,7 +54,7 @@ describe PagesController do
       
        it "should have the right title" do
           get 'help'
-          response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Help")
+          response.should have_selector("title", :content => @base_title + " | Help")
                     end
     end
 
