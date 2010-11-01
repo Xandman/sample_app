b2ddef0c41b2f1243a81233676ab6c2504de4b99
Xandman <agp@arribatel.com>
2010-11-02 02:09:13 +0800

    Added User controller and tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ SampleApp::Application.routes.draw do
   match '/contact', :to =>  'pages#contact'
   match '/about', :to  =>  'pages#about'
   match '/help', :to  =>  'pages#help'
+  match '/signup', :to  => 'users#new'
 
   
 
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -22,5 +22,27 @@ describe "LayoutLinks" do
         response.should have_selector('title', :content  => "Help")
       end
 
+   it "should have a Signup page at '/signup'" do
+     get '/signup'
+     response.should have_selector('title', :content => "Sign Up")
+   end
+  
+  it "should have the right links on the layout" do
+    visit root_path
+    response.should have_selector('title', :content  => "Home")
+    # click_link "Home"
+    #    response.should have_selector('title', :content  => "Home")
+    #    click_link "Sign Up Now!"
+    #    response.should have_selector('title', :content  => "Sign up")
+    click_link "About"
+    response.should have_selector('title', :content  => "About")
+    click_link "Contact"
+    response.should have_selector('title', :content  => "Contact")
+    click_link "Home"
+    response.should have_selector('title', :content  => "Home")
+     click_link "Sign Up Now!"
+     response.should have_selector('title', :content  => "Sign Up")
+     response.should have_selector('a[href="/"]>img')
+  end
   
 end
