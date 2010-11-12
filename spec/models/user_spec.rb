0ba2196686d3db14e1c8f9341d2f4516eb7e8b14
Xandman <agp@arribatel.com>
2010-11-13 00:03:23 +0800

    user model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,51 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before( :each) do
+    @attr = { :name => "Example User", :email => "user@example.com"}
+  end
+  
+  it "should create a new instance given a valid attribute" do
+    User.create!(@attr)
+  end
+  
+  it "should require a name" do
+    no_name_user = User.new( @attr.merge(:name => ""))
+    no_name_user.should_not be_valid
+  end
+  
+  it "should require email" do
+    no_email_user = User.new( @attr.merge(:email =>""))
+    no_email_user.should_not be_valid
+  end  
+ 
+  it "names should not be too long" do
+    long_name = "a" *51
+    long_name_user = User.new(@attr.merge( :name => long_name))
+    long_name_user.should_not be_valid
+  end
+  
+  it "should accept valid emails" do
+    addresses = %w[user@foo.com the_USER@foo.bar.org first.last@foo.jp]
+    addresses.each do |address|
+      valid_email_user = User.new(@attr.merge( :email => address))
+      valid_email_user.should be_valid
+    end
+  end
+    
+  it "should reject invalid emails" do
+    addresses = %w[user@foo,com the_USER_foo.bar.org first.last@foo.]
+    addresses.each do |address|
+      invalid_email_user = User.new(@attr.merge( :email => address))
+      invalid_email_user.should_not be_valid
+    end
+  end
+  
+  it "email should be unique" do
+    User.create!(@attr)
+    user_with_duplicate = User.new(@attr)
+    user_with_duplicate.should_not be_valid
+    
+  end
 end
